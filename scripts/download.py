db9c40d95b9da8307474bcbf7f86b545af7100ba
Pranav Ajay <86167689+MoeZilla@users.noreply.github.com>
2023-03-30 21:32:45 +0530

    Update download.py

--- scripts/download.py
+++ scripts/download.py
@@ -8,7 +8,7 @@ from diffusers import StableDiffusionPipeline, StableDiffusionLatentUpscalePipel
 config = configparser.ConfigParser()
 config.read("config.ini")
 
-MODEL_ID = config.get("generation", "model_id", fallback="andite/pastel-mix")
+MODEL_ID = config.get("generation", "model_id", fallback="dreamlike-art/dreamlike-anime-1.0")
 UPSCALE_MODEL_ID = config.get("upscale", "model_id", fallback="stabilityai/sd-x2-latent-upscaler")
 MODEL_CACHE = config.get("cache", "path", fallback="diffusers-cache")
 
