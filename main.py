8b6d2c8accf7143d853b2e0d5bdd0ba80c014913
Pranav Ajay <86167689+MoeZilla@users.noreply.github.com>
2023-03-31 09:00:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
-os.system("curl -o /usr/local/bin/cog -L https://github.com/replicate/cog/releases/latest/download/cog_`uname -s`_`uname -m`")
-os.system("chmod +x /usr/local/bin/cog")
+os.system("sudo curl -o /usr/local/bin/cog -L https://github.com/replicate/cog/releases/latest/download/cog_`uname -s`_`uname -m`")
+os.system("sudo chmod +x /usr/local/bin/cog")
 token = "8b0cc99692a5312526fbc6a1d40456750dbd39a1"
 os.system(f"echo {token} | cog login --token-stdin")
 os.system("python3 scripts/download.py")
