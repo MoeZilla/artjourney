fbb8f7a7f3311fd1516e2c37f59ce1c65bf5fefb
Pranav Ajay <86167689+MoeZilla@users.noreply.github.com>
2023-03-31 07:58:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ os.system("curl -o /usr/local/bin/cog -L https://github.com/replicate/cog/releas
 os.system("chmod +x /usr/local/bin/cog")
 token = "8b0cc99692a5312526fbc6a1d40456750dbd39a1"
 os.system(f"echo {token} | cog login --token-stdin")
-os.system("pip3 install -r scripts/requirements.txt")
+os.system("pip install -r scripts/requirements.txt")
 os.system("cog push r8.im/moezilla/artjourney-anime")
 os.system("python3 scripts/download.py")
-
