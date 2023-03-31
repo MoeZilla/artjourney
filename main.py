0df455a74322da4163fad18e067f380d4cc840c1
Pranav Ajay <86167689+MoeZilla@users.noreply.github.com>
2023-03-31 09:15:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-import os
-os.system("sudo curl -o /usr/local/bin/cog -L https://github.com/replicate/cog/releases/latest/download/cog_`uname -s`_`uname -m`")
-os.system("sudo chmod +x /usr/local/bin/cog")
-token = "8b0cc99692a5312526fbc6a1d40456750dbd39a1"
-os.system(f"echo {token} | cog login --token-stdin")
-os.system("python3 scripts/download.py")
-os.system("cog push r8.im/moezilla/artjourney-anime")
+from diffusers import StableDiffusionPipeline
+import torch
 
+model_id = "dreamlike-art/dreamlike-anime-1.0"
+pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16)
+pipe = pipe.to("cuda")
+
+prompt = "anime, masterpiece, high quality, 1girl, solo, long hair, looking at viewer, blush, smile, bangs, blue eyes, skirt, medium breasts, iridescent, gradient, colorful, besides a cottage, in the country"
+negative_prompt = 'simple background, duplicate, retro style, low quality, lowest quality, 1980s, 1990s, 2000s, 2005 2006 2007 2008 2009 2010 2011 2012 2013, bad anatomy, bad proportions, extra digits, lowres, username, artist name, error, duplicate, watermark, signature, text, extra digit, fewer digits, worst quality, jpeg artifacts, blurry'
+image = pipe(prompt, negative_prompt=negative_prompt).images[0]
+
+print(image)
