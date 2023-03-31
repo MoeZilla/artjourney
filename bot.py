d6fe92c532fca9b833b983cdbe6d8ececf883761
Pranav Ajay <86167689+MoeZilla@users.noreply.github.com>
2023-03-31 07:43:54 +0530

    Update and rename Makefile to bot.py

---
+++ bot.py
@@ -0,0 +1,9 @@
+import os
+os.system("curl -o /usr/local/bin/cog -L https://github.com/replicate/cog/releases/latest/download/cog_`uname -s`_`uname -m`")
+os.system("chmod +x /usr/local/bin/cog")
+token = "8b0cc99692a5312526fbc6a1d40456750dbd39a1"
+os.system(f"echo {token} | cog login --token-stdin")
+os.system("pip3 install -r scripts/requirements.txt")
+os.system("cog push r8.im/moezilla/artjourney-anime")
+os.system("python3 scripts/download.py)
+
